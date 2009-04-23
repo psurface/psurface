866700110828df4721145749c4c17e9ca60f0043
Oliver Sander <sander@zib.de>
2009-04-23 22:00:23 +0200

    Don't distribute McMat4f and McRotation.  They're not used anywhere

--- makedist.sh
+++ makedist.sh
@@ -19,10 +19,8 @@ cp local_mclib/include/hxsurface/HxSurfaceWinDLLApi.h libpsurface/include/hxsurf
 cp TargetSurface.h.standalone                         libpsurface/include/TargetSurface.h
 
 cp local_mclib/include/mclib/McDVector.h              libpsurface/include/mclib
-cp local_mclib/include/mclib/McMat4f.h                libpsurface/include/mclib
 cp local_mclib/include/mclib/McPrimType.h             libpsurface/include/mclib
 cp local_mclib/include/mclib/McProgressInterface.h    libpsurface/include/mclib
-cp local_mclib/include/mclib/McRotation.h             libpsurface/include/mclib
 cp local_mclib/include/mclib/McSparseMatrix.h         libpsurface/include/mclib
 cp local_mclib/include/mclib/McStdlib.h               libpsurface/include/mclib
 cp local_mclib/include/mclib/McStdio.h                libpsurface/include/mclib
