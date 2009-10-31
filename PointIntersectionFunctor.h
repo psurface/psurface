d812b08a7452bcc0878a71b45f1d5c504103a091
Oliver Sander <sander@zib.de>
2009-10-31 18:10:05 +0100

    Remove unused operator()

--- PointIntersectionFunctor.h
+++ PointIntersectionFunctor.h
@@ -8,11 +8,6 @@
 template <class ctype>
 struct PointIntersectionFunctor
 {
-    bool operator()(const Box<ctype, 3>& box, const StaticVector<ctype,3>* item) const {
-        return (box.lower()[0] <= (*item)[0]) && ((*item)[0] <= box.upper()[0])
-            && (box.lower()[1] <= (*item)[1]) && ((*item)[1] <= box.upper()[1])
-            && (box.lower()[2] <= (*item)[2]) && ((*item)[2] <= box.upper()[2]);
-    }
 
     bool operator()(const std::tr1::array<ctype,3>& lower, 
                     const std::tr1::array<ctype,3>& upper, const StaticVector<ctype,3>& item) const {
