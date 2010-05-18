cc1b9f05b7454aced2e2b825501b36fb157ac9c9
Oliver Sander <sander@zib.de>
2010-05-18 15:20:47 +0200

    Add methods to set up a parametrization.  Not all of them are implemented yet.

--- PSurfaceFactory.cpp
+++ PSurfaceFactory.cpp
@@ -1,7 +1,10 @@
+#include <vector>
+
+#include <psurface/PSurface.h>
 #include <psurface/PSurfaceFactory.h>
 
 template <int dim, class ctype>
-void PSurfaceFactory<dim,ctype>::setTargetSurface(const Surface* surface)
+void PSurfaceFactory<dim,ctype>::setTargetSurface(Surface* surface)
 {
     psurface_->surface = surface;
 }
@@ -15,10 +18,36 @@ void PSurfaceFactory<dim,ctype>::insertVertex(const StaticVector<ctype,dim+1>& p
 template <int dim, class ctype>
 unsigned int  PSurfaceFactory<dim,ctype>::insertSimplex(const std::tr1::array<unsigned int, dim+1>& v)
 {
-    assert(dim==2);
     unsigned int idx = psurface_->createSpaceForTriangle(v[0], v[1], v[2]);
     psurface_->integrateTriangle(idx);
     return idx;
+}
+
+template <int dim, class ctype>
+void PSurfaceFactory<dim,ctype>::insertTargetVertexMapping(unsigned int targetVertex, 
+                                                           unsigned int domainTriangle, 
+                                                           const StaticVector<ctype,dim>& domainLocalPosition)
+{
+}
+
+template <int dim, class ctype>
+void PSurfaceFactory<dim,ctype>::insertGhostNode(unsigned int domainVertex,
+                                                 unsigned int targetTriangle,
+                                                 const StaticVector<ctype,dim>& targetLocalPosition)
+{
+    std::vector<int> neighbors = psurface_->getTrianglesPerVertex(domainVertex);
+    
+    for (int i=0; i<neighbors.size(); i++) {
+        
+        int corner = psurface_->triangles(neighbors[i]).getCorner(domainVertex);
+        psurface_->addGhostNode(neighbors[i], corner, targetTriangle, targetLocalPosition);
+        
+    }
+}
+
+template <int dim, class ctype>
+void PSurfaceFactory<dim,ctype>::insertEdge()
+{
 }
 
 // ///////////////////////////////////////////////////////////////////////
--- PSurfaceFactory.h
+++ PSurfaceFactory.h
@@ -1,8 +1,11 @@
 #ifndef PSURFACE_FACTORY_H
 #define PSURFACE_FACTORY_H
 
+#include <tr1/array>
+
 template <int dim, class ctype> class PSurface;
 class Surface;
+template <class ctype, int dim> class StaticVector;
 
 template <int dim, class ctype>
 class PSurfaceFactory
@@ -13,7 +16,7 @@ public:
         : psurface_(psurface)
     {}
 
-    void setTargetSurface(const Surface* surface);
+    void setTargetSurface(Surface* surface);
 
     void insertVertex(const StaticVector<ctype,dim+1>& position);
 
@@ -22,6 +25,16 @@ public:
     \return The index of the newly inserted triangle
     */
     unsigned int insertSimplex(const std::tr1::array<unsigned int, dim+1>& v);
+
+    void insertTargetVertexMapping(unsigned int targetVertex, 
+                                   unsigned int domainTriangle, 
+                                   const StaticVector<ctype,dim>& domainLocalPosition);
+
+    void insertGhostNode(unsigned int domainVertex,
+                         unsigned int targetTriangle,
+                         const StaticVector<ctype,dim>& targetLocalPosition);
+
+    void insertEdge();
 
 private:
 
