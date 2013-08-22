ecb85f81bfc088a73efe1a1ec40711d2858f57fb
irisxiaoxue <xiaoxue198912@gmail.com>
2013-08-22 04:57:19 -0400

    correct bug in header file

--- vtuIOtest.cpp
+++ vtuIOtest.cpp
@@ -17,21 +17,21 @@
 #include <amiramesh/AmiraMesh.h>
 //#endif
 //vtu file writen by christin
-#include "dune/vtuwriter.hh"
-#include "psurface/AmiraMeshIO.h"
+#include "vtuwriter.hh"
+#include "AmiraMeshIO.h"
 #include <iostream>
 #include "vtuIO.h"
-using namespace Dune;
+using namespace psurface;
 int main(int argc, char **argv)
 {
-  psurface::PSURFACE_API AmiraMeshIO<double> amIO,amIO1,amIO2;
+  PSURFACE_API AmiraMeshIO<double> amIO,amIO1,amIO2;
   const char *file1, *file2,*file3;
   file1  = "cube.par.am";
   file2  = "sphere.par.am";
   file3  = "trice.par.am";
   AmiraMesh* am1 = AmiraMesh::read(file1);
-  psurface::PSurface<2,double>* par1 = new psurface::PSurface<2,double>;
-  psurface::Surface* surf1 = new psurface::Surface;
+  PSurface<2,double>* par1 = new PSurface<2,double>;
+  Surface* surf1 = new Surface;
   if (!amIO.initFromAmiraMesh(par1,am1,file1, surf1)) {
   printf("error in getting psurface from cube.par.am");
   }
@@ -43,8 +43,8 @@ int main(int argc, char **argv)
   }
 
   AmiraMesh* am2 = AmiraMesh::read(file2);
-  psurface::PSurface<2,double>* par2 = new psurface::PSurface<2,double>;
-  psurface::Surface* surf2 = new psurface::Surface;
+  PSurface<2,double>* par2 = new PSurface<2,double>;
+  Surface* surf2 = new Surface;
   if (!amIO1.initFromAmiraMesh(par2,am2,file2, surf2)) {
   printf("error in getting psurface from sphere.par.am");
   }
@@ -56,8 +56,8 @@ int main(int argc, char **argv)
   }
 
   AmiraMesh* am3 = AmiraMesh::read(file3);
-  psurface::PSurface<2,double>* par3 = new psurface::PSurface<2,double>;
-  psurface::Surface* surf3 = new psurface::Surface;
+  PSurface<2,double>* par3 = new PSurface<2,double>;
+  Surface* surf3 = new Surface;
   if (!amIO2.initFromAmiraMesh(par3,am3,file3, surf3)) {
   printf("error in getting psurface from trice.par.am");
   }
