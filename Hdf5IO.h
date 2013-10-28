58eed34b0bd964276c0c7723b130988aa88293e3
Oliver Sander <sander@igpm.rwth-aachen.de>
2013-10-28 16:45:01 +0100

    Remove trailing whitespace

--- Hdf5IO.h
+++ Hdf5IO.h
@@ -7,13 +7,13 @@ namespace psurface{
 template<class ctype,int dim>
 class Hdf5IO{
     private:
-    /// Psurface object hdf5IO deals. 
+    /// Psurface object hdf5IO deals.
     PSurface<dim,ctype>* par;
     /// Global coordinate of nodes
     std::vector<StaticVector<ctype,3> > nodePositions;
     /// Parameter Edge endpoints in global index
     std::vector<StaticVector<int,2> > parameterEdgeArray;
-    
+
     /// Number of triangle vertices
     int numVertices;
     /// Number of base grid triangles
@@ -24,27 +24,27 @@ class Hdf5IO{
     int numParamEdges;
     /// Number of total Edge Points
     int numEdgePoints;
-   
+
     /// Total number of cells
     int ncells;
     /// Total number of points
     int nvertices;
 
     /// Writes the parametrization in hdf5 format with all data we need to read it in paraview.
-    void writeHdf5Data(const std::string& filename);    
-    ///  Writes the parametrization in hdf5 format 
+    void writeHdf5Data(const std::string& filename);
+    ///  Writes the parametrization in hdf5 format
     void writeBaseHdf5Data(const std::string& filename);
 
     public:
     Hdf5IO(PSurface<dim,ctype>* psurface);
 
     /** \brief Writes the parametrization in hdf5 format and create relatd xdmf file
-     *  If base is set to be true(1), we create hdf5 file that only contain necessary data  
-     *  of the psurface. If base is set to be false(0), we create hdf5 file that contain all  
-     *  needed to display it in paraview. 
+     *  If base is set to be true(1), we create hdf5 file that only contain necessary data
+     *  of the psurface. If base is set to be false(0), we create hdf5 file that contain all
+     *  needed to display it in paraview.
      */
     void creatHdfAndXdmf(const std::string&  xdf_filename, const std::string&  hdf_filename, bool base);
-    
+
     /// Writes xdmf file so we can read hdf5 throught paraview
     void writeXdmf(const std::string&  xdf_filename, const std::string&  hdf_filename);
 
