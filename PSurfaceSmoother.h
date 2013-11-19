f2ab848cdf63bf33399d8f104b0c0e1104fe3df1
Oliver Sander <sander@igpm.rwth-aachen.de>
2013-11-19 20:26:27 +0100

    Document method applyEdgeRelaxation

--- PSurfaceSmoother.h
+++ PSurfaceSmoother.h
@@ -16,7 +16,14 @@ class PSURFACE_API PSurfaceSmoother
     
 public:
 
-    ///
+    /** \brief Smooth the parametrization graph across a specific base grid edge
+     *
+     * \param psurface The psurface object to be smoothed
+     * \param edge The edge to smooth
+     * \param keepPatch If this is true, then smoothing on patch boundaries will only be tangential
+     * \param nodeStack Source of temporary memory.  Needs to be given from the outside, to be
+     *          persistent across calls
+     */
     static void applyEdgeRelaxation(PSurface<2,ctype>* psurface, int edge, 
                                     bool keepPatches, std::vector<unsigned int>& nodeStack);
 
